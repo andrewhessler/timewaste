b8e81e811f4d0d201f4f8c86688c7ba17d01ea99
Andrew Hessler <adhessler@gmail.com>
2026-04-05 20:47:13 -0500

    starting again

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,86 @@
+use std::{default, sync::Arc};
+
 use anyhow::Result;
+use wgpu::{
+    Device, Instance, InstanceDescriptor, RequestAdapterOptions, wgc::device, wgt::DeviceDescriptor,
+};
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ControlFlow, EventLoop},
+    window::Window,
+};
+
+#[derive(Default)]
+struct App {
+    state: Option<State>,
+}
+
+struct State {
+    device: Device,
+    window: Arc<Window>,
+}
+
+impl State {
+    async fn new(window: Arc<Window>) -> anyhow::Result<Self> {
+        let instance = Instance::new(&InstanceDescriptor {
+            ..Default::default()
+        });
+
+        let surface = instance.create_surface(window.clone())?;
+
+        let adapter = instance
+            .request_adapter(&RequestAdapterOptions {
+                compatible_surface: Some(&surface),
+                ..Default::default()
+            })
+            .await?;
+
+        let (device, _queue) = adapter
+            .request_device(&DeviceDescriptor {
+                ..Default::default()
+            })
+            .await?;
+
+        Ok(Self { device, window })
+    }
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        let window = Arc::new(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
+
+        self.state = Some(pollster::block_on(State::new(window)).unwrap());
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        window_id: winit::window::WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            WindowEvent::RedrawRequested => {
+                println!("draw");
+            }
+            _ => (),
+        }
+    }
+}
 
 pub fn run() -> Result<()> {
-    println!("Running");
+    env_logger::init();
+    let event_loop = EventLoop::new()?;
+
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let mut app = App::default();
+    event_loop.run_app(&mut app)?;
+
     Ok(())
 }
